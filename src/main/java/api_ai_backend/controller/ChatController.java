7644e45258b1ace0c11558de51273846ebf36e8b
eliasfernandescout <eliasfernandescout@gmail.com>
2025-04-18 21:35:25 -0300

    add to gitignore

--- src/main/java/api_ai_backend/controller/ChatController.java
+++ src/main/java/api_ai_backend/controller/ChatController.java
@@ -1,9 +1,12 @@
 package api_ai_backend.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import reactor.core.publisher.Flux;
 
 
 @RestController
@@ -14,11 +17,25 @@ public class ChatController {
         this.chatClient = chatClientBuilder.build();
     }
 
-    @GetMapping("/ai")
-    String generation(String userInput) {
+    @PostMapping("/ai")
+    String generation(@RequestBody String userInput) {
         return this.chatClient.prompt()
                 .user(userInput)
                 .call()
+                .content();
+    }
+
+    /**
+     * REQUEST COM STREAM DE DADOS
+     */
+
+    @PostMapping(value = "/ai-stream",
+            produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> chat(@RequestBody String userInput) {
+        return chatClient
+                .prompt()
+                .user(userInput)
+                .stream()
                 .content();
     }
 }
