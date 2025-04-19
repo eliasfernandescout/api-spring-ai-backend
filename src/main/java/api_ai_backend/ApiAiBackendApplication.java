7d7c0153c7bf5b1c4ed8553b816cbed76b50d975
eliasfernandescout <eliasfernandescout@gmail.com>
2025-04-18 21:07:49 -0300

    first commit

---
+++ src/main/java/api_ai_backend/ApiAiBackendApplication.java
@@ -0,0 +1,13 @@
+package api_ai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api_ai_backend/ApiAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package api_ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
